b0099e97f9ee3597ab75ce198acce661fce3f607
Gia1987 <giacomo.n27@gmail.com>
2018-09-07 12:38:29 +0100

    Created grid

---
+++ src/grid.js
@@ -0,0 +1,10 @@
+function drawGrid(x,y) {
+  let $gameBoard = $('#myTable');
+  for(let r = (x - 1); r >= 0; r--){
+    let $row = $(`<tr id="${r}"></tr>`)
+    for(let c = 0; c < y; c++){
+      $row.append(`<td class= "${c}"></td>`);
+    }
+    $gameBoard.append($row)
+  }
+}
